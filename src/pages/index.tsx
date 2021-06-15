c15260d5863c4efed718db8c78d6b0524c4f0258
akamt <akamt68@gmail.com>
2021-06-15 15:20:53 +0900

    fusion timeを使って表示してみた。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import dynamic from 'next/dynamic.js'
 const Column2DGraph = dynamic(() => import('../components/column2DGraph'), {
   ssr: false,
 })
+const TimeSeriesGraph = dynamic(() => import('../components/timeSeries'), {
+  ssr: false,
+})
 
 const Title = styled.h1`
   color: red;
@@ -14,6 +17,7 @@ export default function Home() {
     <div>
       <Title>My page</Title>
       <Column2DGraph />
+      <TimeSeriesGraph />
     </div>
   )
 }
