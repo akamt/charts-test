ed666c7a480f8bc3295ff72eb67cf748a71d8164
akamt <akamt68@gmail.com>
2021-06-18 21:17:24 +0900

    x軸が動いたら、他のグラフが追従するように syncExtremes を追加した。

--- src/pages/high-charts.tsx
+++ src/pages/high-charts.tsx
@@ -14,6 +14,38 @@ if (typeof Highcharts === 'object') {
     this.series.chart.tooltip.refresh(this) // Show the tooltip
     this.series.chart.xAxis[0].drawCrosshair(event, this) // Show the crosshair
   }
+
+  Highcharts.syncExtremes = function (e) {
+    const thisChart = this.chart
+
+    if (e.trigger !== 'syncExtremes') {
+      // Prevent feedback loop
+      Highcharts.each(
+        Highcharts.charts,
+        (chart: {
+          (
+            options: Highcharts.Options,
+            callback?: Highcharts.ChartCallbackFunction | undefined,
+          ): Highcharts.Chart
+          (
+            renderTo: string | HTMLElement,
+            options: Highcharts.Options,
+            callback?: Highcharts.ChartCallbackFunction | undefined,
+          ): Highcharts.Chart
+          xAxis?: any
+        }) => {
+          if (chart && chart !== thisChart) {
+            if (chart.xAxis[0].setExtremes) {
+              // It is null while updating
+              chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {
+                trigger: 'syncExtremes',
+              })
+            }
+          }
+        },
+      )
+    }
+  }
 }
 
 const Container = styled.div`
--- src/types/highcharts.d.ts
+++ src/types/highcharts.d.ts
@@ -1,5 +1,9 @@
+import { PointerEventObject } from 'highcharts'
+
 export declare module 'highcharts' {
   interface Point {
     highlight(e: PointerEventObject): void
   }
+
+  function syncExtremes(e: AxisSetExtremesEventObject): void
 }
