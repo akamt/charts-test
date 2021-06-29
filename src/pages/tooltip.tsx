ef8ad57dc3ccfbb3d2647493d8fd699107eb5637
akamt <akamt68@gmail.com>
2021-06-29 16:13:15 +0900

    不要な記述を削除した。

--- src/pages/tooltip.tsx
+++ src/pages/tooltip.tsx
@@ -1,48 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
-import CustomTooltipChart from '../components/highCharts/CustomTooltipChart'
+import HighchartsExporting from 'highcharts/modules/exporting'
 import Highcharts from 'highcharts/highstock'
 
+import CustomTooltipChart from '../components/highCharts/CustomTooltipChart'
+
 if (typeof Highcharts === 'object') {
-  Highcharts.Pointer.prototype.reset = () => {}
-
-  Highcharts.Point.prototype.highlight = function (event) {
-    this.onMouseOver() // Show the hover marker
-    this.series.chart.tooltip.refresh(this) // Show the tooltip
-    this.series.chart.xAxis[0].drawCrosshair(event, this) // Show the crosshair
-  }
-
-  Highcharts.syncExtremes = function (e) {
-    const thisChart = this.chart
-
-    if (e.trigger !== 'syncExtremes') {
-      // Prevent feedback loop
-      Highcharts.each(
-        Highcharts.charts,
-        (chart: {
-          (
-            options: Highcharts.Options,
-            callback?: Highcharts.ChartCallbackFunction | undefined,
-          ): Highcharts.Chart
-          (
-            renderTo: string | HTMLElement,
-            options: Highcharts.Options,
-            callback?: Highcharts.ChartCallbackFunction | undefined,
-          ): Highcharts.Chart
-          xAxis?: any
-        }) => {
-          if (chart && chart !== thisChart) {
-            if (chart.xAxis[0].setExtremes) {
-              // It is null while updating
-              chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {
-                trigger: 'syncExtremes',
-              })
-            }
-          }
-        },
-      )
-    }
-  }
+  HighchartsExporting(Highcharts)
 }
 
 const Container = styled.div`
@@ -54,26 +18,10 @@ const Container = styled.div`
   flex-wrap: wrap;
 `
 
-const HighChartsPage = () => {
-  // const handleMouseMove = (e: React.MouseEvent<HTMLElement>) => {
-  //   let point = null
-  //   let event = null
-  //
-  //   Highcharts.charts.forEach((chart) => {
-  //     if (!chart) return
-  //     event = chart.pointer.normalize(e as unknown as PointerEvent) // Find coordinates within the chart
-  //     point = chart.series[0].searchPoint(event, true) // Get the hovered point
-  //     if (point) {
-  //       point.highlight(event)
-  //     }
-  //   })
-  // }
+const CustomTooltipPage = () => (
+  <Container>
+    <CustomTooltipChart />
+  </Container>
+)
 
-  return (
-    <Container>
-      <CustomTooltipChart />
-    </Container>
-  )
-}
-
-export default HighChartsPage
+export default CustomTooltipPage
